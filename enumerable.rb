9b6fe0eeb37666e56a705600cde8cbf9f0274fa6
calvinoea <calvinebunamu@gmail.com>
2020-04-27 10:34:49 +0100

    my_each and my_each_with_index methods made for enumerable module

--- enumerable.rb
+++ enumerable.rb
@@ -1,5 +1,30 @@
 module Enumerable
   def my_each
-    # your code here
+    return to_enum :my_each unless block_given?
+    i = 0
+
+    while i < self.size
+      yield self[i]
+      i+=1
+    end
   end
+
+  def my_each_with_index
+    return to_enum :my_each unless block_given?
+
+    i = 0
+    while i < self.size
+      yield(self[i],i)
+      i += 1
+    end
+  end
+
+
 end
+
+
+array = [5,4,6,7]
+
+
+puts array.my_each {|h| puts "#{h} is the guy"}
+puts array.my_each_with_index{|h| puts "#{h} is the index"}
