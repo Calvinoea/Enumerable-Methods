cecb4772a15797ae69c223cd8e5a740d929e853f
calvinoea <calvinebunamu@gmail.com>
2020-05-11 16:48:52 +0100

    Some errors removed

--- spec/enumerable_spec.rb
+++ spec/enumerable_spec.rb
@@ -1,4 +1,6 @@
-#spec/spec.rb
+# require 'rspec/autorun'
+#spec/enumerable_spec.rb
+# RSpec.describe Enumerable do
 require_relative '../enumerable.rb'
 
 
@@ -12,7 +14,7 @@ describe '#my_each' do
   it 'loops through items of the array' do
    expect(numbers.my_each { |element| element}).to eql(numbers)
  end
-end
+
 
  it 'loops through array of strings' do
     expect(string.my_each {|element| element}).to eql(string)
@@ -29,20 +31,29 @@ describe '.my_each_with_index' do
    numbers.my_each_with_index {|n, i| result << "#{n}: #{i}"}
    expect(result).to eql(['1:0', '2:1', '3:2', '4:3'])
 end
+
  it 'returns Enumerator when block is not passed' do
     expect(numbers.my_each_with_index).to be_a(Enumerator)
  end
 end
 
 describe '.my_select' do
-  it 'returns items when true is returned to block'
+it 'returns items that return true to the block' do
+      output = numbers.select { |n| n > 2 }
+      expect(numbers.my_select { |n| n > 2 }).to eql(output)
+ end
+
+
+  it 'returns items when true is returned to block' do
     expect(numbers.my_select).to be_a(Enumerator)
 end
 
 it 'filter array' do
   expect(numbers.my_select(&:even?)).to eql([0])
-  end
+ end
 end
+
+
 
 describe '.my_all' do
   it 'still scans the array if there is no block' do
@@ -59,10 +70,12 @@ describe '#my_any' do
   expect(nil_array.my_any?).to be(true)
 end
 
+
 it 'works even if array is empty' do
   expect(empty.my_any?).to be(false)
-  end
-end
+      end
+
+
 
 describe 'my_none' do
   it 'Goes through array even if no block is given' do
@@ -76,44 +89,30 @@ end
 
 describe '.my_inject' do
   it 'returns the result of the operation on elements' do
-    expect((5..10).my_inject(:*)).to eql((5..10)).inject(:*))
+    expect((5..10).my_inject(:*)).to eql((5..10).inject(:*))
   end
-end
+
 
 it 'returns result of operations with first value' do
   output = (5..10).inject(2, :*)
-  expect((5..10)).my_inject(2, :*)).to eql(output)
+  expect((5..10).my_inject(2, :*)).to eql(output)
   end
-end
+
 
 it 'returns the result of all operations on elements' do
   output = numbers.inject {|item,next_item| item * next_item}
   expect(numbers.my_inject {|item, next_item|}).to eql(output)
  end
-end
+
 
 it 'returns accumulation of operations on all items, with first value' do
-  output = numbers.inject(2) {|item, next_item|}).to eql(output)
-  expect(numbers.my_inject(2) {|item, next_item|}).to eql(output)
-  end
+  output = numbers.inject(2) {|item, next_item| item * next_item}
+  expect(numbers.my_inject(2) {|item, next_item| item * next_item}).to eql(output)
 end
 
 describe 'multiply_els' do
-  it 'tests my_inject'
-    expect(multiply_els([2,4,5])).to be(40)
-   end
+  it 'tests my_inject' do
+    expect(multiply_els([5,4,1])).to be(40)
   end
- end
 end
-
-
-
-
-
-
-
-
-
-
-
-
+end
